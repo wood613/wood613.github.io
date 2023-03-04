c0a3e6fdc81ab0d0541795fdad0aeb9bcb2a7012
wood613 <91185605+wood613@users.noreply.github.com>
2023-03-04 21:17:48 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+    // 模拟加载时间，这里设置为3秒钟
+    setTimeout(function() {
+        // 隐藏loading界面
+        document.querySelector(".loading").style.display = "none";
+        // 显示主界面
+        document.querySelector("body").style.visibility = "visible";
+    }, 3000);
+});
